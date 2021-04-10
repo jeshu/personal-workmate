86d764e82db207ae6e5cff98d6d777b2b94c6792
Jeshu Brij <jeshu911@gmail.com>
2021-04-11 00:11:09 +0530

    animations wip

--- lib/components/form_box.dart
+++ lib/components/form_box.dart
@@ -1,45 +1,26 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
 
-class FormBox extends StatefulWidget {
-  // const FormBox({Key key, this.animationController}) : super(key: key);
+class FormBox extends StatelessWidget {
+  final double opacity;
+  final double angle;
+  final double size;
 
-  @override
-  _FormBoxState createState() => _FormBoxState();
-}
-
-class _FormBoxState extends State<FormBox> with SingleTickerProviderStateMixin {
-  AnimationController _animationController;
-  Animation _opacity;
-  Animation _angle;
-
-  @override
-  void initState() {
-    super.initState();
-    _animationController = AnimationController(
-        vsync: this, duration: Duration(milliseconds: 3000));
-
-    _opacity = Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
-        parent: _animationController,
-        curve: Interval(0.0, 200.0, curve: Curves.easeInCirc)));
-
-    // _angle = Tween(begin: 0, end: (pi / 4) * 100).animate(CurvedAnimation(
-    //     parent: _animationController,
-    //     curve: Interval(200, 400, curve: Curves.easeInCirc)));
-
-    _animationController.forward();
-    setState(() {});
-  }
+  const FormBox(
+      {Key key,
+      @required this.opacity,
+      @required this.angle,
+      @required this.size})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Opacity(
-      opacity: _opacity.value,
+      opacity: opacity,
       child: Transform.rotate(
-        angle: pi / 4, //_angle.value.toDouble() / 100,
+        angle: angle, //_angle.value.toDouble() / 100,
         child: SizedBox(
-          width: MediaQuery.of(context).size.height * 0.6,
-          height: MediaQuery.of(context).size.height * 0.6,
+          width: size,
+          height: size,
           child: Container(
             decoration: BoxDecoration(
               color: Colors.white,
--- lib/screens/login_screen/login_screen.dart
+++ lib/screens/login_screen/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 import 'package:personal_workmate/components/form_box.dart';
 import 'package:personal_workmate/components/hero_logo.dart';
 import 'package:personal_workmate/screens/login_screen/login_form.dart';
@@ -10,7 +11,36 @@ class LoginScreen extends StatefulWidget {
   _LoginScreenState createState() => _LoginScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _LoginScreenState extends State<LoginScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+  Animation _opacity;
+  Animation _angle;
+  Animation _size;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 2));
+
+    _opacity = Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+        parent: _animationController,
+        curve: Interval(0.0, 0.3, curve: Curves.easeInExpo)));
+
+    _angle = Tween(begin: pi / 4, end: 9 * (pi / 4)).animate(CurvedAnimation(
+        parent: _animationController,
+        curve: Interval(0.2, 0.9, curve: Curves.decelerate)));
+    _size = Tween(begin: 0.0, end: 455.0).animate(CurvedAnimation(
+        parent: _animationController,
+        curve: Interval(0.2, 0.9, curve: Curves.decelerate)));
+
+    _animationController.forward();
+    _animationController.addListener(() {
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -28,7 +58,11 @@ class _LoginScreenState extends State<LoginScreen> {
         Positioned(
           left: MediaQuery.of(context).size.width * -0.4,
           top: MediaQuery.of(context).size.height * 0.25,
-          child: FormBox(),
+          child: FormBox(
+            opacity: _opacity?.value ?? 0,
+            angle: _angle?.value ?? 0,
+            size: _size?.value ?? 0,
+          ),
         ),
         Positioned(
           width: MediaQuery.of(context).size.width * 0.7,
